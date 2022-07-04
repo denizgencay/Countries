e48905e242c3b802d067af15500ed6e9dad958b8
denizgencay <denizgencay35@gmail.com>
2022-07-04 21:40:15 +0300

    Detail fragment bottom navigation view's manipulated.

--- app/src/main/java/com/example/countries/ui/country_detail/CountryDetailFragment.kt
+++ app/src/main/java/com/example/countries/ui/country_detail/CountryDetailFragment.kt
@@ -5,18 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.countries.R
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.countries.MainActivity
+import com.example.countries.databinding.FragmentCountryDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CountryDetailFragment : Fragment() {
 
+    private lateinit var binding: FragmentCountryDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentCountryDetailBinding.inflate(inflater,container,false)
+        (activity as MainActivity).hideNavigationBar()
+        handleAppBarClickEvents()
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_country_detail, container, false)
+    private fun handleAppBarClickEvents(){
+        binding.backButtonDetail.setOnClickListener{
+            (activity as MainActivity).showNavigationBar()
+            findNavController().popBackStack()
+        }
     }
 
 }
